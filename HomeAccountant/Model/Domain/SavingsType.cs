dc88c09c0e15c2aa963a9cdb8d9edfb80befe321
z003tdtd <z003tdtd@EVH05558NB>
2017-05-29 08:47:37 +0200

    SavingsType table without dependencies (FK)

---
+++ HomeAccountant/Model/Domain/SavingsType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAccountant.Model.Domain
+{
+    class SavingsType
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual float Percentage { get; set; }
+        public virtual decimal Savings { get; set; }
+    }
+}

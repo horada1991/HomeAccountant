79890548550a880a6e18bcb64cbbc055feefd8fd
z003tdtd <z003tdtd@EVH05558NB>
2017-05-29 09:04:44 +0200

    SavingsTypes connected to Account table

--- HomeAccountant/Model/Domain/Account.cs
+++ HomeAccountant/Model/Domain/Account.cs
@@ -10,5 +10,12 @@ namespace HomeAccountant.Model.Domain
     {
         public virtual Guid Id { get; set; }
         public virtual decimal AvailableFunds { get; set; }
+        public ISet<SavingsType> savingsTypes = new HashSet<SavingsType>();
+
+        public ISet<SavingsType> SavingsTypes
+        {
+            get { return savingsTypes; }
+            set { savingsTypes = value; }
+        }
     }
 }
